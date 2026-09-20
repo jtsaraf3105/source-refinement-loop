4ef2e6114f9096e5de569818b2a62ca269c41ec1
Jatin K. Saraf <94530145+jtsaraf3105@users.noreply.github.com>
2026-09-20 15:07:39 +0530

    Increase OPENAI_TIMEOUT from 40 to 90 seconds

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ class Settings:
         self.openai_api_key: str = os.getenv("OPENAI_API_KEY", "")
         self.openai_model: str = os.getenv("OPENAI_MODEL", "gpt-4o-mini")
         self.top_n: int = int(os.getenv("TOP_N_CANDIDATES", "5"))
-        self.openai_timeout: float = float(os.getenv("OPENAI_TIMEOUT", "40"))
+        self.openai_timeout: float = float(os.getenv("OPENAI_TIMEOUT", "90"))
 
 
 @lru_cache
